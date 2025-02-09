bfe9e7644f7088e0a80df8542b6d52e33ea2850a
WilfredoFC <wfelizcaba7@gmail.com>
2025-02-09 11:48:49 +0800

    Crear entidades InsuranceProvider, NetworkType y Specialties

---
+++ NetMed.Domain/Entities/NetworkType.cs
@@ -0,0 +1,15 @@
+﻿using NetMed.Domain.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetMed.Domain.Entities
+{
+    public sealed class NetworkType : BaseEntity<int>
+    {
+        [Column("NetworkTypeID")]
+        [Key]
+        public override int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
