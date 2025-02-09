da0d93c48cc0f9589378a01f07e2c16635e99db6
Cristian Beltre <cristianbeltre10@gmail.com>
2025-02-09 00:35:42 -0400

    Agregar mis entidades(En mi rama)

---
+++ NetMed.Domain/Entities/Users.cs
@@ -0,0 +1,17 @@
+﻿using NetMed.Domain.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetMed.Domain.Entities
+{
+    public sealed class Users : PersonEntity
+    {
+        [Column("UserID")]
+        [Key]
+        public override int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public int RoleID { get; set; }
+    }
+}
