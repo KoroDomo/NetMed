bca6b088e421f01645cc58d1e524282a900149be
Dioris Arias <20242@itla.edu.do>
2025-02-09 21:58:24 -0400

    Reemplzar entidades por correctas del modulo Medical

---
+++ NetMed.Domain/Entities/AvailabilityModes.cs
@@ -0,0 +1,14 @@
+﻿using NetMed.Domain.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetMed.Domain.Entities
+{
+    public sealed class AvailabilityModes : BaseEntity<int> 
+    {
+        [Column("SAvailabilityModeID")]
+        [Key]
+        public override int Id { get; set; }
+        public string AvailabilityModeName { get; set; }
+    }
+}
